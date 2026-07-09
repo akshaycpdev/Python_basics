b6f3d22222998fff512f6d5e55ef6584de2224ae
Akshay C P <akshaycpaiml06@gmail.com>
2026-07-09 23:05:22 +0530

    Intoduction

---
+++ chapter1.py
@@ -0,0 +1,135 @@
+print("hello")
+
+print("My Name is Akshay, I'm 21 year old")
+
+
+
+print(25 + 26)
+
+name = "Akshay"
+age = 21
+price = 25.5
+
+print(type(name),name)
+print(type(age),age)
+print(type(price))
+
+#datatypes
+
+name1 = "Akshay"
+name2 = ' C P '
+name3 = '''Akshay'''
+
+print(name1)
+print(name2)
+print(name3)
+
+#sum
+print(sum([1,2,4]))
+
+#arithmetic operations
+
+a=10
+b=3
+
+print(a+b) # addition
+print(a-b) # subtraction
+print(a*b) # multiplication
+print(a/b) # division
+print(a//b) # floor division
+print(a**b) # cube root (a^b)
+print(a%b) # difference
+
+#relational operators
+a=10
+b=5
+a == b
+print(b)
+print(a==b) #Boolean value True or False
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assignment operators
+num = 10
+num = num + 10 #10+10=20
+num += 10 #20+10=30
+num *= 10 
+print(num)
+
+
+#type conversion
+a =2
+b = 3.5
+sum = a + b
+print(sum)
+print(type(b))
+
+#type casting
+a = int("2")
+b = 4.25
+
+print(a+b)
+print(type(a))
+
+#Questions with Answers
+
+#1. Basic Printing
+# Write a Python program to print:
+print("Python Programming\nI am learning Python from basics\nDay 1 Completed")
+
+
+# 2. Student Details
+# Create variables for:
+# Student name
+# Roll number
+# Age
+# CGPA
+# Print all details with proper messages.
+name = "Akshay"
+roll_no = 5
+age = 21
+CGPA = 7.0
+
+print("My name is: ",name)
+print("Age: ",age)
+print("Roll Number: ",roll_no)
+print("CGPA: ",CGPA)
+
+
+# 3. Arithmetic Operations
+# Create two variables:
+
+# a = 50
+# b = 8
+
+# Find and print:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor division
+# Remainder
+# Power
+
+a = 50
+b = 8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# 4. Area Calculation
+# Create a variable for the length and breadth of a rectangle.
+# Calculate and print:
+# Area
+# Perimeter
+
+length = 30
+breadth = 40
+print(2*(length*breadth))
