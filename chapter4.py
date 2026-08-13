e300a7c976ec499b16c47c21c923e91d254c016c
Akshay C P <choutajeakshay@gmail.com>
2026-08-13 23:03:13 +0530

    Dictionary

---
+++ chapter4.py
@@ -0,0 +1,11 @@
+dict = {
+    "name": "Akshay",
+    "Subjects": ["C Program", "Python", "Java", "ADA", "DSA"],
+    "Marks": [90, 98, 85, 94, 75],
+    "CGPA": 9.32,
+}
+
+print(dict)
+print(dict["name"])
+print(dict["Marks"])
+print(dict["CGPA"])
